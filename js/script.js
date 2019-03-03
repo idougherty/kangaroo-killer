5038a1aba4e8bde7631e31c4b6775cdef9204df2
idougherty <idougherty@cps.edu>
2019-03-03 06:03:31 -0600

    everything

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 var canvas = document.getElementById("canvas");
 var c = canvas.getContext("2d");
+woodInfo = document.getElementById("wood");
+metalInfo = document.getElementById("metal");
+stoneInfo = document.getElementById("stone");
+iqInfo = document.getElementById("iq");
+foodInfo = document.getElementById("food");
+populationInfo = document.getElementById("population");
+militaryInfo = document.getElementById("military");
+turnInfo = document.getElementById("turn");
+govInfo = document.getElementById("government");
+actionInfo = document.getElementById("action-points");
+workerInfo = document.getElementById("workers");
+bnameInfo = document.getElementById("bname");
+unemployedInfo = document.getElementById("unemployed");
 
 const events = [
 	"whale",
@@ -25,38 +38,39 @@ function fightBadEv(cost) {
 }
 
 function runTurn() {
+	gameState.actionPoints = 3;
 	gameState.turnCount += 1;
 	for (const hex of activeHexes) {
 		switch (hex.state.type) {
 			case "farm":
-				gameState.food += 5;
+				gameState.food += 5*hex.state.workers/3;
 				break;
 			case "factory":
-				gameState.metal += 5;
+				gameState.metal += 5*hex.state.workers/3;
 				break;
 			case "mine":
-				gameState.stone += 5;
+				gameState.stone += 5*hex.state.workers/3;
 				break;
 			case "mill":
-				gameState.wood += 5;
+				gameState.wood += 5*hex.state.workers/3;
 				break;
 			case "school":
-				gameState.iq += 5;
+				gameState.iq += 5*hex.state.workers/3;
 				break;
 			case "university":
-				gameState.iq += 10;
+				gameState.iq += 10*hex.state.workers/5;
 				break;
 			case "barracks":
 				gameState.militaryBuildUp = Math.min(
-					100,
+					50,
 					gameState.militaryBuildUp + 5
 				);
 				break;
 			case "simpleHousing":
-				gameState.population += 5;
+				gameState.population += 1;
 				break;
 			case "superHousing":
-				gameState.population += 10;
+				gameState.population += 3;
 				break;
 			case "nuclearFacilities":
 				// TODO: win something
@@ -85,7 +99,6 @@ function runTurn() {
 			gameState.metal *= 0.5;
 			gameState.iq *= 0.5;
 			gameState.militaryBuildUp *= 0.5;
-			gameState.food *= 0.5;
 		}
 	} else if (rand < 0.5) {
 		const eventIdx = Math.floor(rand * 2 * events.length);
@@ -116,7 +129,7 @@ function runTurn() {
 				gameState.population *= 1.15;
 				break;
 			case "meth":
-				gameState.population *= 1.15;
+				gameState.productionModifier = 1.25;
 				break;
 			case "patriotism":
 				gameState.extraMilitary += 30;
@@ -128,6 +141,74 @@ function runTurn() {
 				throw new Error("event not implemented");
 		}
 	}
+	
+	gameState.population = Math.floor(gameState.population);
+	gameState.food = Math.floor(gameState.food);
+	gameState.wood = Math.floor(gameState.wood);
+	gameState.stone = Math.floor(gameState.stone);
+	gameState.metal = Math.floor(gameState.metal);
+	gameState.iq = Math.floor(gameState.iq);
+	gameState.militaryBuildUp = Math.floor(gameState.militaryBuildUp);
+	draw();
+}
+
+function updateStats() {
+	foodInfo.innerHTML = gameState.food;
+	stoneInfo.innerHTML = gameState.stone;
+	woodInfo.innerHTML = gameState.wood;
+	metalInfo.innerHTML = gameState.metal;
+	iqInfo.innerHTML = gameState.iq;
+	populationInfo.innerHTML = gameState.population;
+	militaryInfo.innerHTML = gameState.military;
+	turnInfo.innerHTML = gameState.turnCount;
+	govInfo.innerHTML = gameState.govt;
+	actionInfo.innerHTML = gameState.actionPoints;
+	if(gameState.selectedHex != null) {
+		var bname = g.Hexes[gameState.selectedHex].state.type;
+		bnameInfo.innerHTML = bname.charAt(0).toUpperCase()+bname.substring(1, bname.length)+":";
+		workerInfo.innerHTML = g.Hexes[gameState.selectedHex].state.workers;
+		unemployedInfo.innerHTML = gameState.population - gameState.employed;
+	}
+}
+
+function moveWorkers(amt) {
+	var curHex = g.Hexes[gameState.selectedHex].state;
+	if(curHex.workers + amt >= 0 && gameState.employed + amt <= gameState.population) {
+		switch (curHex.type) {
+			case "farm":
+			case "factory":
+			case "mine":
+			case "mill":
+			case "school":
+				if(curHex.workers + amt <= 3) {
+					curHex.workers += amt;
+					gameState.employed += amt;
+				}
+				break;
+			case "university":
+				if(curHex.workers + amt <= 5) {
+					curHex.workers += amt;
+					gameState.employed += amt;
+				}
+				break;
+			case "barracks":
+				if(curHex.workers + amt <= 10) {
+					curHex.workers += amt;
+					gameState.employed += amt;
+				}
+				break;
+			case "cityHall":
+			case "nuclearFacilities":
+				if(curHex.workers + amt <= 1) {
+					curHex.workers += amt;
+					gameState.employed += amt;
+				}
+				break;
+			default:
+				throw new Error(`Unknown building type ${curHex.type}`);
+		}
+	}
+	draw();
 }
 
 function draw() {
@@ -136,27 +217,69 @@ function draw() {
 	for (const hex of activeHexes) {
 		drawHex(hex);
 	}
+	updateStats();
 }
 
 function drawHex(/* HT.Hexagon */ hex) {
-	if (!hex.selected) {
-		c.strokeStyle = "#ccc";
-		c.lineWidth = 1;
-	} else {
-		c.strokeStyle = "black";
-		c.lineWidth = 3;
-	}
-
+	c.strokeStyle = "#5abcd8";
+	c.lineWidth = 3;
+	
 	c.beginPath();
 	c.moveTo(hex.Points[0].X, hex.Points[0].Y);
 	for (var i = 1; i < hex.Points.length; i++) {
 		var p = hex.Points[i];
 		c.lineTo(p.X, p.Y);
 	}
-
 	c.closePath();
-	c.fillStyle = "#008000";
+	
+	switch(hex.state.type) {
+		case "farm":
+			c.fillStyle = "brown";
+			break;
+		case "factory":
+			c.fillStyle = "lightgrey";
+			break;
+		case "mine":
+			c.fillStyle = "#696969";
+			break;
+		case "mill":
+			c.fillStyle = "#8B4513";
+			break;
+		case "school":
+			c.fillStyle = "#003366";
+			break;
+		case "university":
+			c.fillStyle = "#013220";
+			break;
+		case "barracks":
+			c.fillStyle = "red";
+			break;
+		case "simpleHousing":
+			c.fillStyle = "orange";
+			break;
+		case "superHousing":
+			c.fillStyle = "#CC5500";
+			break;
+		case "cityHall":
+			c.fillStyle = "white";
+			break;
+		case "nuclearFacilities":
+			c.fillStyle = "black";
+			break;
+		case "none":
+			c.fillStyle = "green";
+			break;
+		default:
+			throw new Error(`Unknown building type ${hex.state.type}`);
+	}
+	
 	c.fill();
+	
+	if(hex.state.selected) {
+		c.fillStyle = "rgba(255, 255, 255, .3)";
+		c.fill();
+	}
+	
 	c.stroke();
 
 	if (false && hex.Id) {
@@ -192,15 +315,18 @@ function drawHex(/* HT.Hexagon */ hex) {
 
 class GameState {
 	constructor() {
-		this.turnCount = 0;
+		this.actionPoints = 3;
+		this.turnCount = 1;
 		this.govt = "anarchy";
 		this.productionModifier = 1;
+		this.selectedHex = null;
 
-		// materials
-		this.population = 0;
-		this.food = 0;
-		this.wood = 0;
-		this.stone = 0;
+		//materials
+		this.population = 5;
+		this.employed = 0;
+		this.food = 10;
+		this.wood = 10;
+		this.stone = 10;
 		this.metal = 0;
 		this.iq = 0;
 		this.militaryBuildUp = 0;
@@ -223,11 +349,18 @@ class GameState {
 
 function CellState() {
 	this.workers = 0;
-	this.type = "farm";
+	this.type = "none";
+	this.selected = false;
 }
 
 CellState.prototype.click = function(id) {
+	for(const hex of activeHexes) {
+		hex.state.selected = false;
+	}
+	
 	this.selected = true;
+
+	gameState.selectedHex = id;
 };
 
 canvas.addEventListener("click", ({ layerX, layerY }) => {
@@ -237,13 +370,112 @@ canvas.addEventListener("click", ({ layerX, layerY }) => {
 	draw();
 });
 
+function buildButton(building) {
+	if(gameState.actionPoints > 0 && g.Hexes[gameState.selectedHex].state.type == "none") {
+		var isGucci = false;
+		switch(building) {
+			case "farm":
+				if(gameState.stone >= 10 && gameState.wood >= 10) {
+					isGucci = true;
+					gameState.stone -= 10;
+					gameState.wood -= 10;
+				}
+				break;
+			case "factory":
+				if(gameState.stone >= 40 && gameState.iq >= 15 && gameState.population >= 50) {
+					isGucci = true;
+					gameState.stone -= 40;
+					gameState.iq -= 15;
+				}
+				break;
+			case "mine":
+				if(gameState.wood >= 10) {
+					isGucci = true;
+					gameState.wood -= 10;
+				}
+				break;
+			case "mill":
+				if(gameState.stone >= 10) {
+					isGucci = true;
+					gameState.stone -= 10;
+				}
+				break;
+			case "school":
+				if(gameState.stone >= 20 && gameState.wood >= 10 && gameState.population >= 30) {
+					isGucci = true;
+					gameState.stone -= 20;
+					gameState.wood -= 10;
+				}
+				break;
+			case "university":
+				if(gameState.stone >= 30 && gameState.metal >= 30 && gameState.iq >= 15) {
+					isGucci = true;
+					gameState.stone -= 20;
+					gameState.wood -= 10;
+					gameState.iq -= 10;
+				}
+				break;
+			case "barracks":
+				if(gameState.stone >= 30 && gameState.iq >= 15 && gameState.metal >= 20 && gameState.population >= 30) {
+					isGucci = true;
+					gameState.stone -= 30;
+					gameState.iq -= 15;
+					gameState.metal -= 20;
+				}
+				break;
+			case "simpleHousing":
+				if(gameState.stone >= 10 && gameState.wood >= 10) {
+					isGucci = true;
+					gameState.stone -= 10;
+					gameState.wood -= 10;
+				}
+				break;
+			case "superHousing":
+				if(gameState.stone >= 40 && gameState.iq >= 15 && gameState.metal >= 30) {
+					isGucci = true;
+					gameState.stone -= 40;
+					gameState.iq -= 15;
+					gameState.metal -= 30;
+				}
+				break;
+			case "cityHall":
+				if(gameState.stone >= 15 && gameState.wood >= 15 && gameState.population >= 20) {
+					isGucci = true;
+					gameState.stone -= 15;
+					gameState.wood -= 15;
+				}
+				break;
+			case "nuclearFacilities":
+				if(gameState.stone >= 60 && gameState.iq >= 70 && gameState.metal >= 90 && gameState.wood >= 20 && gameState.population >= 60) {
+					isGucci = true;
+					gameState.stone -= 30;
+					gameState.iq -= 15;
+					gameState.metal -= 20;
+				}
+				break;
+			default:
+				throw new Error(`Unknown building type ${hex.state.type}`);
+		}
+		
+		if(isGucci) {
+			gameState.actionPoints--;
+			g.Hexes[gameState.selectedHex].state.type = building;
+		}
+		draw();
+	}
+}
+
+function updateHexInfo(hexState) {
+	
+}
+
 var g = new HT.Grid(canvas.width, canvas.height);
 
 var activeHexes = g.Hexes.filter((_hex, idx) => map.includes(idx));
 
 g.Hexes.forEach((_hex, i) => {
 	if (!map.includes(i)) {
-		// delete g.Hexes[i]
+		delete g.Hexes[i]
 	}
 });
 
